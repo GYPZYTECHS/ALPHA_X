f6c345475136cf758b788c842a03c386fbc7d682
GYPZY_TECH <99523108+GYPZYTECHS@users.noreply.github.com>
2022-06-17 08:45:22 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GYPZYTECHS/ALPHA_X
+  git clone https://github.com/DKBOTx/TovinoBotV3.git /TovinoBotV3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO https://github.com/GYPZYTECHS/ALPHA_X
+  git clone $UPSTREAM_REPO /TovinoBotV3
 fi
-cd https://github.com/GYPZYTECHS/ALPHA_X
+cd /TovinoBotV3
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
 python3 bot.py
+
